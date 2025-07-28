3d6e2fc579bb27ec3e4dbba21f55024bff96877e
Sedan <sedangu@gmail.com>
2025-07-28 09:27:15 -0300

    subiendo git

---
+++ MiSolucion/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Ejercicio3
+{
+  static public class Program
+  {
+    public static void Main(string [] args)
+    {
+      Console.WriteLine("Hola mundo!");
+      Console.WriteLine("Presione una tecla para salir.");
+      Console.ReadKey(); 
+    }
+  }
+}
